8f38f505f180c0c7e2eeca0d77d2d260efb7edcb
Adeniyi Odunuga <Adeniyi Odunuga>
2025-03-20 21:06:55 +0100

    updated

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -15,18 +15,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
       Position? position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high,
       );
-
     }
 
     return Scaffold(
       body: Center(
         child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Color(0xFF3C3C3C)),
+          ),
           onPressed: () {
             getLocation();
           },
-          child: Text('Get Location'),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Get Location',style: TextStyle(fontSize: 30),),
+          ),
         ),
       ),
+
     );
   }
 }
