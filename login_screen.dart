9e7ccf0d1226eb51e3c6bd551812ede964e4f7e8
Adeniyi Odunuga <odunugaadeniyi@gmail.com>
2025-03-20 08:30:08 +0100

    Climate App

---
+++ login_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatelessWidget {
+  const LogInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(onPressed: (){},
+            child: Text('Get Location')),
+      ),
+      );
+
+  }
+}
